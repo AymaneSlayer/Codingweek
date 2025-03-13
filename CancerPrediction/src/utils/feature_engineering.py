e30c9b45eff9876b42ba289c2a214e12b84de6f1
walid <walid3035@icloud.com>
2025-03-13 21:08:12 +0000

    Annuler

--- CancerPrediction/src/utils/feature_engineering.py
+++
@@ -1,8 +0,0 @@
-#Fonctions pour l'extraction et la création de caractéristiques.
-def encode_categorical_features(data):
-    # Encoder les variables catégorielles
-    data = pd.get_dummies(data, drop_first=True)
-    return datadef encode_categorical_features(data):
-    # Encoder les variables catégorielles
-    data = pd.get_dummies(data, drop_first=True)
-    return data
