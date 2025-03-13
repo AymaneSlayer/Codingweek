689497b0306835b4a2c1b728a8551bccf804aec5
AymaneSlayer <aymane.falaki@centrale-casablanca.ma>
2025-03-13 12:30:33 +0000

    Update dataloader.py

--- CancerPrediction/src/dataloader.py
+++ CancerPrediction/src/dataloader.py
@@ -1,1 +1,16 @@
+import pandas as pd
+class CancerDataLoader:
+    def __init__(self, file_path):
+        self.data = pd.read_csv(file_path)
+
+    def get_features_labels(self):
+        X = self.data.iloc[:, :-1]  # All columns except last (features)
+        y = self.data.iloc[:, -1]   # Last column (label)
+        return X.values, y.values
+
+# Example Usage
+file_path = r"C:\Users\MSI\Desktop\data.csv"
+loader = CancerDataLoader(file_path)
+X, y = loader.get_features_labels()
+print(X.shape, y.shape)
 
