cfb9afc81d701dc8f905bc8e37dbe4b27c6c163f
Bambin0000 <bambinodianka@gmail.com>
2025-03-13 13:36:06 +0000

    Update evaluation.py

--- CancerPrediction/src/utils/evaluation.py
+++ CancerPrediction/src/utils/evaluation.py
@@ -1,1 +1,8 @@
+#Métriques pour évaluer les performances des modèles
+from sklearn.metrics import accuracy_score, roc_auc_score
 
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    roc_auc = roc_auc_score(y_test, y_pred)
+    return accuracy, roc_auc
